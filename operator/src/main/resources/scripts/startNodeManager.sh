ae7bc0f0ae73eae852671768168dcc2c62cd0d66
Robert Donat <robert.donat@oracle.com>
2019-01-29 13:38:53 -0500

    no injection for introspector pod, add 5556 port to service for servers, change node manager ListenAddress=0.0.0.0

--- operator/src/main/resources/scripts/startNodeManager.sh
+++ operator/src/main/resources/scripts/startNodeManager.sh
@@ -175,7 +175,7 @@ cat <<EOF > ${nm_props_file}
   JavaHome=${JAVA_HOME}
   LogLevel=FINEST
   DomainsFileEnabled=true
-  ListenAddress=${SERVICE_NAME}
+  ListenAddress=0.0.0.0
   NativeVersionEnabled=true
   ListenPort=5556
   LogToStderr=true
