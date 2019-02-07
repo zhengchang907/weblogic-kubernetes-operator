9ef9bc7cf4bef0605301ff62bdd0e9b3b2f6116e
Mark Nelson <mark.x.nelson@oracle.com>
2019-02-07 17:43:06 -0500

    initial commit

---
+++ cluster-manager/src/main/java/oracle.kubernetes.clustermanager/Main.java
@@ -0,0 +1,12 @@
+// Copyright 2019, Oracle Corporation and/or its affiliates. All rights reserved.
+
+package oracle.kubernetes.clustermanager;
+
+/** Cluster manager lets you get an ephemeral Kubernetes cluster for testing the operator. */
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("Cluster Manager");
+  }
+}
