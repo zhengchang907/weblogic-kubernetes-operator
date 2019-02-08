d5cb399dd0cb42a98ac00997eedd8c076855c18c
Mark Nelson <mark.x.nelson@oracle.com>
2019-02-07 21:18:53 -0500

    take maven executable path from env, if present (for jenkins)

--- cluster-manager/download-oci-sdk.sh
+++ cluster-manager/download-oci-sdk.sh
@@ -1,16 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 echo 'Downloading the OCI Java SDK...'
 wget -q https://github.com/oracle/oci-java-sdk/releases/download/v1.3.7/oci-java-sdk.zip
 
+echo 'Extracting the library...'
 jar xvf oci-java-sdk.zip lib/oci-java-sdk-full-1.3.7.jar
 
-mvn install:install-file \
-    -Dfile=lib/oci-java-sdk-full-1.3.7.jar \
-    -DgeneratePom=true \
-    -DgroupId=com.oracle.oci \
-    -DartifactId=java-sdk \
-    -Dversion=1.3.7 \
-    -Dpackaging=jar
+if [[ "$MAVEN" == "" ]]; then
+  export MAVEN="mvn"
+fi
+
+echo 'Installing into local Maven repository...'
+$MAVEN install:install-file \
+       -Dfile=lib/oci-java-sdk-full-1.3.7.jar \
+       -DgeneratePom=true \
+       -DgroupId=com.oracle.oci \
+       -DartifactId=java-sdk \
+       -Dversion=1.3.7 \
+       -Dpackaging=jar
 
 rm lib/oci-java-sdk-full-1.3.7.jar oci-java-sdk.zip
+
+echo 'Done'
