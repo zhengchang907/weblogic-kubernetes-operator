f7d6ddbbd24086eef2b517bf9d4133823e0eca0d
Mark Nelson <mark.x.nelson@oracle.com>
2020-04-09 16:50:48 -0400

    checkstylegit add integration-tests/!

--- integration-tests/src/test/java/oracle/kubernetes/operator/BaseTest.java
+++ integration-tests/src/test/java/oracle/kubernetes/operator/BaseTest.java
@@ -25,7 +25,6 @@ import oracle.kubernetes.operator.utils.ExecResult;
 import oracle.kubernetes.operator.utils.LoggerHelper;
 import oracle.kubernetes.operator.utils.Operator;
 import oracle.kubernetes.operator.utils.TestUtils;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * Base class which contains common methods to create/shutdown operator and domain. IT tests can
